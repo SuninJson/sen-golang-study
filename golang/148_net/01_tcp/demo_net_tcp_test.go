fa2c295bba0423959d363375e4e7dff12a2d1bb5
sen.huang@github <1538356302@qq.com>
2024-05-14 18:55:26 +0800

    feat(网络编程): 测试

---
+++ golang/148_net/01_tcp/demo_net_tcp_test.go
@@ -0,0 +1,25 @@
+package _1_tcp
+
+import (
+	"testing"
+)
+
+func TestStartServer(t *testing.T) {
+	StartServer()
+}
+
+func TestTcpServerFormat(t *testing.T) {
+	TcpServerFormat()
+}
+
+func TestTcpClientFormat(t *testing.T) {
+	TcpClientFormat()
+}
+
+func TestTcpServerHeartBeat(t *testing.T) {
+	TcpServerHeartBeat()
+}
+
+func TestTcpClientHeartBeat(t *testing.T) {
+	TcpClientHeartBeat()
+}
